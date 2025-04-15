82ee1fe508a5cdbf7ec3b9a5712f818e6198370f
Physix <159613996+1physix@users.noreply.github.com>
2025-04-15 21:23:14 +0000

    First attempt at scraping

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
 
+"""
+Three steps:
+1. Access the page
+2. Break the page down
+3. Find the data you want
+"""
+
+
+
+#Accessing the page:
+url = "https://www.linkedin.com/in/ishaqhussain1/"
+response = requests.get(url)
+html = response.text
+
+#Breaking the HTML down into components (parsing):
+soup = BeautifulSoup(html, "html.parser")
+image = soup.find("src", id = "ember719")
+
+print(image)
